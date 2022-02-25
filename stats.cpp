7395ed19f734669b63fa6b5b2d3cb76e17fac626
bipin-raju-007 <100402419+bipin-raju-007@users.noreply.github.com>
2022-02-25 13:31:45 +0530

    Update 1

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,28 @@
 #include "stats.h"
+using namespace Statistics;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats ComputeStatistics(const std::vector<double>& list_of_numbers) {
     //Implement statistics here
+    Stats stat;
+
+    double sum_of_numbers = 0;
+    double min = list_of_numbers[0];
+    double max = list_of_numbers[0];
+
+    for (auto number: list_of_numbers) {
+        sum_of_numbers += number;
+        if (number < min)
+            min = number;
+        if (number > max)
+            max = number;
+    }
+
+    if (list_of_numbers.size() == 0) {
+        return stat;
+    } else {
+        stat.average = sum_of_numbers/list_of_numbers.size();
+        stat.min = min;
+        stat.max = max;
+        return stat;
+    }
 }
