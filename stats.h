3df6235c0f7d8356b6e061f4afadf2a2a0f207f5
bipin-raju-007 <100402419+bipin-raju-007@users.noreply.github.com>
2022-02-25 16:50:48 +0530

    Update 2
    stats.h

--- stats.h
+++ stats.h
@@ -2,15 +2,48 @@
 #include <math.h>
 
 namespace Statistics {
-    class Stats {
-    public:
-        double average, max, min;
-        Stats() {
-            average = NAN;
-            max = NAN;
-            min = NAN;
-        }
-    };
+class Stats {
+public:
+    double average, max, min;
+    Stats() {
+        average = NAN;
+        max = NAN;
+        min = NAN;
+    }
+};
 
-    Stats ComputeStatistics(const std::vector<double>& );
+Stats ComputeStatistics(const std::vector<double>& );
 }
+
+class IAlerter {
+public:
+    virtual void setAlert(bool isAlert) = 0;
+};
+
+class EmailAlert : public IAlerter {
+public:
+    bool emailSent = false;
+    virtual void setAlert(bool isAlert) {
+        emailSent = isAlert;
+    }
+};
+
+class LEDAlert : public IAlerter {
+public:
+    bool ledGlows = false;
+    virtual void setAlert(bool isAlert) {
+        ledGlows = isAlert;
+    }
+};
+
+class StatsAlerter {
+    float maxThreshold;
+    std::vector<IAlerter*> alerts;
+public:
+    StatsAlerter(float maxThreshold, std::vector<IAlerter*>& alerts) {
+        this->maxThreshold = maxThreshold;
+        this->alerts = alerts;
+    }
+
+    void checkAndAlert(const std::vector<double>& );
+};
